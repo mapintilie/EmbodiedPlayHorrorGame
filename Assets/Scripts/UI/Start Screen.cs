715c120ccdf730cd051fface991b44ab52d755cb
mapintilie <s2410872023@fhooe.at>
2026-06-01 22:37:21 +0200

    Fixed Restart

--- Assets/Scripts/UI/Start Screen.cs
+++ Assets/Scripts/UI/Start Screen.cs
@@ -1,27 +1,54 @@
+// csharp
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class StartScreen : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Tooltip("If true, try to destroy known persistent singletons before loading the game scene.")]
+    public bool destroyKnownSingletonsBeforeLoad = true;
 
     public void SwitchToGameScene()
     {
-        SceneManager.LoadScene(1); //switch to game scene aka sample scene
+        if (destroyKnownSingletonsBeforeLoad)
+        {
+            // Beispiel: TobiiManager als bekannter Singleton (nur wenn in Projekt vorhanden)
+            var tobii = FindObjectOfType<TobiiManager>();
+            if (tobii != null)
+                Destroy(tobii.gameObject);
+
+            // Falls ihr eigene persistenten Manager unter einem GameObject gruppiert habt:
+            var persistentRoot = GameObject.Find("PersistentManagers");
+            if (persistentRoot != null)
+                Destroy(persistentRoot);
+        }
+
+        StartCoroutine(LoadGameSceneCoroutine());
+    }
+
+    private IEnumerator LoadGameSceneCoroutine()
+    {
+        // Asynchron im Single‑Modus laden (ersetzt aktuelle Szene)
+        var ao = SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
+        ao.allowSceneActivation = true;
+
+        while (!ao.isDone)
+            yield return null;
+
+        // Optional: Ressourcen freigeben, damit alles sauber neu initialisiert wird
+        Resources.UnloadUnusedAssets();
+        System.GC.Collect();
     }
 
     public void SwitchToCreditScene()
     {
-        SceneManager.LoadScene(2); //switch to credit scene
+        SceneManager.LoadScene(2);
     }
-    
+
     public void SwitchToStartScene()
     {
-        SceneManager.LoadScene(0); //switch to start scene
+        SceneManager.LoadScene(0);
     }
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+    void Update() { }
 }
